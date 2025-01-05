3247678a7cad4b7256c5e87cb651c4e12716caf5
Julia Preobrazhenskaia <gekata33@gmail.com>
2025-01-05 17:43:24 +0100

    api methods

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,7 +3,11 @@ package tests;
 import data.Login;
 import data.User;
 import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
@@ -15,11 +19,17 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
 public class BaseTest {
     private static final String APP_USER = "преображенская_11@gmail.com";
     private static final String APP_PASSWORD = "123456789";
 
     protected static Login testLogin = new Login(APP_USER, APP_PASSWORD);
+
+    protected String authToken;
 
     protected BrowserType browserType;
 
@@ -45,6 +55,65 @@ public class BaseTest {
         driver = WebDriverFactory.createDriver(browserType);
         driver.get("https://stellarburgers.nomoreparties.site/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Step("Генерация данных уникального пользователя")
+    protected User getRandomUser() {
+        // Генерация слйчайных данных для пользователя
+        String username = RandomStringUtils.randomAlphanumeric(8, 15);  // уникальное имя
+        String password = RandomStringUtils.randomAlphanumeric(8, 15);  // стандартный пароль
+        String email = RandomStringUtils.randomAlphanumeric(8, 15).toLowerCase() + "@yandex.ru";  // email пользователя
+
+        return new User(email, password, username);
+    }
+
+    @Step("Регистрация пользователя: {0}")
+    protected void createDefinedUser(User user) {
+        Assert.assertNotNull("Пользователь для тестов не задан", user);
+
+        ValidatableResponse response = given()
+                .filter(new AllureRestAssured())
+                .header("Content-type", "application/json")
+                .body(user)
+                .log().all()
+                .when()
+                .post("/auth/register")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("success", equalTo(true))  // Успех
+                .body("user.email", equalTo(user.getEmail()))  // Проверка email
+                .body("user.name", equalTo(user.getName()))  // Проверка имени
+                .body("accessToken", notNullValue())  // Проверка наличия токенов
+                .body("refreshToken", notNullValue());
+
+        // Сохраняем токен для дальнейших тестов
+        authToken = response.extract().path("accessToken");
+    }
+
+    protected void deleteDefinedUser(User user) {
+        if (authToken != null) {
+            deleteActiveUser(user);
+        }
+    }
+
+    @Step("Удаление пользователя: {0}")
+    protected void deleteActiveUser(User user) {
+        String authValue = authToken;
+
+        authToken = null;
+
+        given()
+                .filter(new AllureRestAssured())
+                .header("Authorization", authValue)  // Авторизация с использованием токена
+                .log().all()
+                .when()
+                .delete("/auth/user")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(202);  // Успешное удаление
     }
 
     @Step("Регистрация пользователя: {0}")
